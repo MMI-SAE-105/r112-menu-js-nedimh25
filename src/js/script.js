87c43f8653cd5d1e5accbeabbf0a9a25a5ac9679
nedim <nedim14.halepovic@gmail.com>
2024-11-15 10:58:43 +0100

    suite du tp

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 document.querySelector(".menu-btn").addEventListener("click", function () {
     const isOpen = this.getAttribute("aria-expanded") === "true";
     const isClosed = !isOpen;
-    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    console.log("Le menu est fermé : ", isOpen, "Le menu est ouvert : ", isClosed);
 
     const nav = document.querySelector(".menu");
 
     // Alterner les attributs ARIA pour ouvrir ou fermer le menu
-    nav.setAttribute("aria-hidden", isOpen ? "true" : "false"); 
-    this.setAttribute("aria-expanded", isOpen ? "false" : "true"); 
+    nav.ariaHidden = isOpen ; 
+    this.setAttribute("aria-expanded", isClosed ); 
 });
